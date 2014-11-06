acdb1554b2679d0492ac678126cca45bc1f13da7
Antonio Santos <antonio@santosvelasco.com>
2014-11-06 11:00:50 +0100

    Styles and javascripts

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,6 @@
-//= require_tree .
+//= require jquery
+//= require fastclick
+//= require foundation/js/foundation/foundation
+//= require foundation/js/foundation/foundation.topbar
+
+$(document).foundation();
